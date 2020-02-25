cdb0f77fb6d87cef8877ec00ff09942e360b5ca9
Sanjula <sanjula8@gmail.com>
2020-02-24 21:52:04 -0600

    created connection and host

--- cli.js
+++ cli.js
@@ -1,3 +1,26 @@
 const mysql = require("mysql2");
 const inquirer = require("inquirer");
 const cTable = require("console.table");
+
+async function queryDb() {
+	const connection = await mysql.createConnection({
+		host: "localhost",
+
+		port: 3306,
+
+		user: "root",
+
+		password: "",
+
+		database: "company_db"
+	});
+	const [myResults] = connection.query("select * from employees");
+	console.table(myResults);
+}
+
+// connection.connect(function(err) {
+// 	if (err) throw err;
+// 	runSearch();
+// });
+
+// function runSearch() {}
