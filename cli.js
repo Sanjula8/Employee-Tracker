7afcfe91bbf3e50d603faf7e65a2addc73d6a2d3
Sanjula <sanjula8@gmail.com>
2020-02-24 21:58:02 -0600

    added inquirer prompts and switch case

--- cli.js
+++ cli.js
@@ -2,25 +2,72 @@ const mysql = require("mysql2");
 const inquirer = require("inquirer");
 const cTable = require("console.table");
 
-async function queryDb() {
-	const connection = await mysql.createConnection({
-		host: "localhost",
+const connection = mysql.createConnection({
+	host: "localhost",
 
-		port: 3306,
+	port: 3306,
 
-		user: "root",
+	user: "root",
 
-		password: "",
+	password: "",
 
-		database: "company_db"
-	});
-	const [myResults] = connection.query("select * from employees");
-	console.table(myResults);
+	database: "company_db"
+});
+
+connection.connect(function(err) {
+	if (err) throw err;
+	runSearch();
+});
+
+function runSearch() {
+	inquirer
+		.prompt({
+			type: "list",
+			name: "action",
+			message: "What would you like to do?",
+			choices: [
+				"View all employees",
+				"View all departments",
+				"View all managers",
+				"Add Employee",
+				"Add Department",
+				"Add Role",
+				"Remove Employee",
+				"Update Employee Role",
+				"Update Employee Manager",
+				"Exit"
+			]
+		})
+		.then(function(answer) {
+			console.log(answer.action);
+			switch (answer.action) {
+				case "View all employees":
+					employeeView();
+					break;
+
+				case "View all departments":
+					departmentView();
+					break;
+				case "View all managers":
+					managerView();
+					break;
+
+				case "Add Employee":
+					employeeAdd();
+					break;
+				case "Add Department":
+					departmentAdd();
+					break;
+
+				case "Add Role":
+					roleAdd();
+					break;
+				case "Remove Employee":
+					employeeRemove();
+					break;
+				case "Exit":
+					connection.end();
+					break;
+			}
+		});
 }
-
-// connection.connect(function(err) {
-// 	if (err) throw err;
-// 	runSearch();
-// });
-
-// function runSearch() {}
